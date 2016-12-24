138bd0a3c9971d73bf7f3dac18ef28b3749e4271
Kaj <kaaiman99@hotmail.com>
2016-12-24 23:22:15 +0100

    Begin van de hairstyles switchen

---
+++ Assets/HairstyleController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HairstyleController : MonoBehaviour {
+	[SerializeField]
+	private List<GameObject> hairstyles = new List<GameObject>();
+	public BoxCollider _col;
+	private int currentPosList;
+	public GameObject hairstyleInst;
+	private GameObject curHair;
+
+
+	void Start () {
+		curHair = Instantiate (hairstyles [currentPosList], _col.transform.position, Quaternion.identity) as GameObject;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Destroy (curHair);
+			curHair = Instantiate (hairstyles [currentPosList], _col.transform.position, Quaternion.identity) as GameObject;
+			currentPosList += 1;
+		}
+		if (currentPosList >= hairstyles.Count) {
+			currentPosList = 0;
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Destroy (curHair);
+			curHair = Instantiate (hairstyles [currentPosList], _col.transform.position, Quaternion.identity) as GameObject;
+			currentPosList -= 1;
+			if (currentPosList < 0) {
+				currentPosList = hairstyles.Count - 1;
+			}
+		}
+	}
+}
