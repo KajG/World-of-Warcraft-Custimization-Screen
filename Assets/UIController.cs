1ecbf1931853ea7c24be78b1cf95f9ac5958d448
KajG <kaaiman99@hotmail.com>
2017-01-12 14:00:42 +0100

    Scrollbar werkt soort van, moet nu nog select ding zichtbaar maken dat je je select ziet weetjwel
    dan moet ik de linker UI maken, en dan animaties van de char, dan alles
    in elkaar zetten

---
+++ Assets/UIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	public List<GameObject> buttons = new List<GameObject>();
+	private List<Vector3> positions = new List<Vector3> ();
+	private float planePos;
+	private float scrollbarValue;
+	public int speed = 40;
+	public Scrollbar scrollbar;
+	private Vector3 position;
+
+	void Start () {
+		for(int i = 0; i < buttons.Count; i++){
+		positions.Add (buttons [i].transform.position);
+		}
+		scrollbar.value = 0.5f;
+	}
+
+	void Update () {
+		scrollbarValue = scrollbar.value - 0.5f;
+		for (int i = 0; i < buttons.Count; i++) {
+			planePos = (scrollbarValue * speed) + positions[i].y;
+			positions [i] = new Vector3 (positions [i].x, planePos, positions [i].z);
+			buttons [i].transform.position = positions [i];
+		}
+	}
+	public void OnRelease(){
+		scrollbar.value = 0.5f;
+	}
+}
